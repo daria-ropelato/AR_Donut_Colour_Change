fff9db97dedfa0e527295898407337e7a61a55c9
daria-ropelato <ropelato.ch@gmail.com>
2025-08-03 21:28:16 +0200

    new commit

---
+++ AR_Donut_Colour_Change/Assets/Scripts/DonutTracker.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class DonutTracker : MonoBehaviour
+{
+    public GameObject donutPrefab;
+    private GameObject donutInstance;
+
+    void OnEnable()
+    {
+        var trackedImages = FindObjectOfType<ARTrackedImageManager>();
+        trackedImages.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    void OnDisable()
+    {
+        var trackedImages = FindObjectOfType<ARTrackedImageManager>();
+        trackedImages.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach (var image in args.added)
+        {
+            if (donutInstance == null)
+            {
+                donutInstance = Instantiate(donutPrefab, image.transform);
+                donutInstance.transform.localPosition = Vector3.zero;
+
+                // Fix rotation
+                donutInstance.transform.localEulerAngles = new Vector3(90, 0, 0);
+            }
+        }
+    }
+}
